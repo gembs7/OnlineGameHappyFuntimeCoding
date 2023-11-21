8e0fd792ee56d0bedb081c58a410ff62a3cd63cc
Nick Gembs <ng334@cornell.edu>
2023-11-21 01:14:04 -0500

    no more fun

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 from _thread import *
 import sys
+from player import Player
+import pickle
 
 server = "192.168.1.7"
 port = 5555
@@ -17,24 +19,18 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for connection, yo")
 
-def read_pos(str):
-  str = str.split(",")
-  return (float(str[0]), float(str[1]))
 
-def make_pos(tup):
-  return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0,0), (100,100)]
+players = [Player(0,0,50,50,(255,0,0)), Player(100,100,50,50,(0,255,0))]
 
 def threaded_client(conn, player):
   
-  conn.send(str.encode(make_pos(pos[player]))) 
+  conn.send(pickle.dumps(players[player]))
   reply = ""
   while True:
     try:
       #increase size if truanced errors or something else cringe
-      data = read_pos(conn.recv(2048).decode())
-      pos[player] = data
+      data = pickle.loads(conn.recv(2048))
+      players[player] = data
 
 
       if not data:
@@ -42,13 +38,13 @@ def threaded_client(conn, player):
         break
       else:
         if player == 1:
-          reply = pos[0]
+          reply = players[0]
         else:
-          reply = pos[1]
+          reply = players[1]
         print("Recieved: ", data)
         print("Sending: ", reply)
 
-      conn.sendall(str.encode(make_pos(reply)))
+      conn.sendall(pickle.dumps(reply))
     except:
       break
   print("Connection Gonzo")
